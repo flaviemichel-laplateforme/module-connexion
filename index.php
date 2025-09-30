70060f1ad3104987ebb6dd72f3411b85888df00d
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-09-30 15:37:55 +0200

    rajout texte accueil

--- index.php
+++ index.php
@@ -3,7 +3,10 @@ include 'structure/header.php'; // Inclut le header
 ?>
 
 <main>
-
+    <div class="container">
+        <h1>Bienvenue sur mon site de vente en ligne</h1>
+        <p>Mon site de e-commerce spécialisé dans la vente de végétaux.</p>
+    </div>
 </main>
 
 <?php
