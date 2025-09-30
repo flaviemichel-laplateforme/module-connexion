163b65859db839c2afe68c4521982b41d09a49aa
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-09-30 16:18:05 +0200

    creation de mon formulaire d'inscription stylisé sans php pour le moment

--- pages/inscription.php
+++ pages/inscription.php
@@ -4,6 +4,32 @@ include '../structure/header.php'; // Inclut le header
 
 <main>
 
+
+
+    <form class="formulaire_inscription" action="" method="post">
+        <h1 class="titreh1">
+            <img src="../assets/img/logo.png" alt="Logo" class="logo-titre">
+            S'inscrire ici
+        </h1>
+        <label for="login">Login:</label>
+        <input placeholder="Choisir un nom utilisateur" type="text" id="login" name="login" required>
+
+        <label for="prenom">Prénom:</label>
+        <input placeholder="Prénom" type="text" id="prenom" name="prenom" required>
+
+        <label for="nom">Nom:</label>
+        <input placeholder="Nom" type="text" id="nom" name="nom" required>
+
+        <label for="password">Mot de passe:</label>
+        <input placeholder="Mot de passe" type="password" id="password" name="password" required>
+
+        <label for="confirm_password">Confirmez le mot de passe:</label>
+        <input placeholder="Confirmez le mot de passe" type="password" id="confirm_password" name="confirm_password" required>
+
+        <button>S'inscrire</button>
+
+    </form>
+
 </main>
 
 <?php
