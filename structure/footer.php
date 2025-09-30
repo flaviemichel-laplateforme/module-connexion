2c8631a070859de43910fe821ab775018925b997
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2025-09-30 14:25:37 +0200

    modification index db footer header

---
+++ structure/footer.php
@@ -0,0 +1,19 @@
+<link rel="stylesheet" href="../assets/css/style.css">
+
+
+<footer>
+    <div class="container">
+        <a href="https://github.com/flaviemichel-laplateforme"><img src="../assets/img/github.jpg" alt="GitHub"></a>
+    </div>
+
+    <div>
+        <p>Merci de votre visite et à très bientôt sur notre site !
+            Votre satisfaction est notre priorité.
+            © 2025 – Tous droits réservés.</p>
+    </div>
+
+    <div class="container2">
+        <a href="#"><img src="../assets/img/linkedin.png" alt="LinkedIn"></a>
+    </div>
+
+</footer>
---
+++ structure/header.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Module Connexion</title>
+    <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+
+<body>
+    <header class="nav-menu">
+        <div>
+            <img src="../assets/img/logo.png" alt="Logo" class="logo">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="pages/connexion.php">Connexion</a></li>
+                <li><a href="pages/inscription.php">Inscription</a></li>
+                <li><a href="pages/profil.php">Profil</a></li>
+                <li><a href="pages/admin.php">Admin</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="container"> <!-- Container principal -->
+</body>
